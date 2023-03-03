1eb3713b77b59f62226fef41ce486db78466aa44
Carlos Aguilar <hey@carlos-aguilar.com>
2023-03-03 10:07:09 +0100

    chore: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="-mt-32 text-9xl">⌛</h1>
+<h2 class="text-8xl font-bold">coming soon...</h2>
+
+<a
+  href="mailto:hey@carlos-aguilar.com"
+  class="text-7xl text-amber-400 no-underline"
+>
+  hey@carlos-aguilar.com
+</a>
