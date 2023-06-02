f39ff14661a8e5e02014d0f75524bce463deb5ec
Carlos Aguilar <hey@carlos-aguilar.com>
2023-06-02 03:44:54 +0200

    feat: missing translations and new images

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import { i18n } from '$lib/i18n'
   import { Job } from '$lib/components'
-  import work from '$lib/data/work.json'
 </script>
 
-<h1>{$i18n.work.title}</h1>
+<h1>{$i18n.cv.title}</h1>
 <h5 class="text-center">
-  {$i18n.work.description}
+  {$i18n.cv.description}
 </h5>
 
 <div class="mt-16 flex flex-wrap items-stretch">
-  {#each work as job}
+  {#each $i18n.cv.jobs as job}
     <div class="basis-full p-4 xl:basis-1/2">
       <Job {job} />
     </div>
