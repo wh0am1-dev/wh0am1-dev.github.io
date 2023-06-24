f5d021f9ec23c517893d3f15622dace9f9528dd5
Carlos Aguilar <hey@carlos-aguilar.com>
2023-06-24 05:27:55 +0200

    feat: add card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let href: string
+</script>
+
+<a {href} target="_blank" rel="noopener noreferrer">
+  <section
+    class="
+      h-full rounded-md
+      border border-stone-400
+      p-4 shadow-md transition-all
+      dark:border-stone-700 dark:shadow-stone-900
+      md:p-8
+    "
+  >
+    <slot />
+  </section>
+</a>
+
+<style lang="postcss">
+  @media (hover: hover) {
+    section {
+      @apply hover:scale-105 hover:shadow-lg;
+      @apply hover:ring-2 hover:ring-amber-500 dark:hover:ring-amber-400;
+    }
+  }
+</style>
