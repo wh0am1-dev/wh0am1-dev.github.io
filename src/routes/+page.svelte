9dd8a38aafb7b2ea004d4ea3b6b60a00ea060d54
Carlos Aguilar <hey@carlos-aguilar.com>
2023-06-24 05:29:50 +0200

    chore: remove ego component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
   import { i18n } from '$lib/i18n'
-  import { Ego, Contact } from '$lib/components'
+  import { Contact } from '$lib/components'
 </script>
 
-<Ego />
+<img
+  alt="ego"
+  src="/img/ego.png"
+  class="
+    aspect-square w-64 rounded-md
+    border-4 border-amber-500 bg-amber-500 object-cover shadow-md
+    dark:border-amber-400 dark:bg-amber-400 dark:shadow-stone-900
+    xl:w-80
+  "
+/>
 
 <h1 class="my-8">
   {$i18n.home.wave}
