b68db82860ca3c801259ea04cc7d07444d5510eb
Carlos Aguilar <hey@carlos-aguilar.com>
2023-06-24 05:30:54 +0200

    style: add wave animation 👋

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,12 @@
   "
 />
 
-<h1 class="my-8">
-  {$i18n.home.wave}
+<h1 class="relative my-8">
+  {#each [...$i18n.home.wave] as character, index}
+    <span class="rotate" style="animation-delay: {index * 0.2}s">
+      {character}
+    </span>
+  {/each}
 </h1>
 
 <p class="max-w-2xl px-4 text-center">
@@ -25,3 +29,23 @@
 <hr />
 
 <Contact />
+
+<style lang="postcss">
+  @media (hover: hover) {
+    img {
+      @apply transition-all hover:scale-105 hover:shadow-lg;
+    }
+  }
+
+  .rotate {
+    @apply relative inline-block;
+    animation: flip 5s infinite;
+  }
+
+  @keyframes flip {
+    0%,
+    80% {
+      transform: rotateY(360deg);
+    }
+  }
+</style>
