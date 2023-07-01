5055a7c1eaf6f0c0b831b80170303cd93a26d7e3
Carlos Aguilar <hey@carlos-aguilar.com>
2023-07-01 06:25:16 +0200

    styles: polish shadows

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -3,25 +3,23 @@
 </script>
 
 <a {href} target="_blank" rel="noopener noreferrer">
-  <section
-    class="
-      h-full rounded-md
-      border border-stone-400
-      p-4 shadow-md transition-all
-      dark:border-stone-700 dark:shadow-stone-900
-      md:p-8
-    "
-  >
+  <section>
     <slot />
   </section>
 </a>
 
 <style lang="postcss">
+  section {
+    @apply h-full rounded-md p-4 transition-all md:p-8;
+    @apply shadow-sm shadow-stone-400 dark:shadow-stone-950;
+    @apply border border-stone-400 dark:border-stone-700;
+  }
+
   @media (hover: hover) {
     section {
-      @apply hover:scale-105 hover:shadow-lg;
-      @apply hover:ring-2 hover:ring-amber-500;
-      @apply dark:hover:shadow-stone-900 dark:hover:ring-amber-400;
+      @apply hover:shadow-md hover:shadow-stone-400;
+      @apply hover:scale-105 hover:ring-2 hover:ring-amber-500;
+      @apply dark:hover:shadow-stone-950 dark:hover:ring-amber-400;
     }
   }
 </style>
