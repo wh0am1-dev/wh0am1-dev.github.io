2bb160fdc00863c88e83a77af0c84d41f57be8e1
Carlos Aguilar <hey@carlos-aguilar.com>
2023-07-01 06:22:51 +0200

    fix: use absolute paths

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,12 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      precompress: true,
+    }),
+    paths: {
+      relative: false,
+    },
   },
 }
 
