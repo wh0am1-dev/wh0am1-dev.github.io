fcc638aa68a3aa85485440ded23ccf1820abc561
Carlos Aguilar <hey@carlos-aguilar.com>
2023-07-05 04:41:44 +0200

    feat: add workshops to cv

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { i18n } from '$lib/i18n'
-  import { Job } from '$lib/components'
+  import { Job, Workshop } from '$lib/components'
 </script>
 
 <svelte:head>
@@ -16,6 +16,21 @@
   {#each $i18n.cv.jobs as job}
     <div class="basis-full p-4 lg:basis-1/2">
       <Job {job} />
+    </div>
+  {/each}
+</div>
+
+<hr />
+
+<h1>{$i18n.cv.workshops.title}</h1>
+<h5 class="text-center">
+  {$i18n.cv.workshops.description}
+</h5>
+
+<div class="mt-16 flex flex-wrap items-stretch">
+  {#each $i18n.cv.workshops.list as workshop}
+    <div class="basis-full p-4 lg:basis-1/2">
+      <Workshop {workshop} />
     </div>
   {/each}
 </div>
